ea55466a145271c9c1fea4a1b5efb155ace07fd0
태현 <78199616+NekoNyangYee@users.noreply.github.com>
2025-03-20 21:42:56 +0900

    add: cicd 메시지 테스트

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -5,7 +5,6 @@ const jwt = require("jsonwebtoken");
 const { pool } = require("../config/db");
 
 router.post("/signup", async (req, res) => {
-    console.log("📌 req.body:", req.body); // 디버깅용 로그 추가
 
     const { username, email, password } = req.body;
     if (!username || !email || !password) {
